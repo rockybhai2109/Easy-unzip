7ede37b108bd518536addab052f76ccc0cd453b3
Bharathraj M <139947099+Bharathraj-Muthukumaran@users.noreply.github.com>
2025-06-22 23:24:35 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Install system dependencies
-apt-get update && apt-get install -y unrar
-
-# Install Python dependencies
-pip install -r requirements.txt
