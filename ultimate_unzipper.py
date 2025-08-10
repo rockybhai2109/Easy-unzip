375ec86d95e50b8d67e2cd1eff59b0d1a9d63eea
rockybhai2109 <r.kingbhai786@gmail.com>
2025-08-10 12:44:14 +0530

    Update ultimate_unzipper.py

--- ultimate_unzipper.py
+++ ultimate_unzipper.py
@@ -322,7 +322,7 @@ class UltimateBot:
     async def start_command_handler(self, client: Client, message: types.Message):
         """Handles the /start command."""
         await self.send_text(message.chat.id, (
-            "👋 Hello! Send me a `.zip`, `.7z`, or `.rar` archive and I'll unzip it and let you choose which files to download."
+            "👋 Hoi! Send me a `.zip`, `.7z`, or `.rar` archive and I'll unzip it and let you choose which files to download."
         ))
 
     async def document_handler(self, client: Client, message: types.Message):
